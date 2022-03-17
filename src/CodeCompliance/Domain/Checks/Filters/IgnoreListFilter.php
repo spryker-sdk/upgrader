3eebdb0cb7d70052c4b4f533d32300d5344c78e1
v.strelchenko <v.strelchenko@gamingtec.com>
2022-03-17 12:00:00 +0200

    PPLUS-853: Updated ignore list

--- src/CodeCompliance/Domain/Checks/Filters/IgnoreListFilter.php
+++ src/CodeCompliance/Domain/Checks/Filters/IgnoreListFilter.php
@@ -73,7 +73,7 @@ class IgnoreListFilter implements FilterInterface
             '/\/\w+ConfigurationProvider$/',
             '/\/Development\//',
             '/^Spryker\/Zed\/\w+DataImport\//',
-            '/\/Twig\TwigFunctionProvider$/'
+            '/\/Twig\TwigFunctionProvider$/',
         ];
     }
 }
