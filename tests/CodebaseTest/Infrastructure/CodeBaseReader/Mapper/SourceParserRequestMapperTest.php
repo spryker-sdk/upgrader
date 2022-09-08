fbcbc96f59182a63016c649968fc9d4ea544fbfa
v.strelchenko <v.strelchenko@gamingtec.com>
2022-09-08 14:47:08 +0300

    Added ModuleOptionMapperMapperTest

--- tests/CodebaseTest/Infrastructure/CodeBaseReader/Mapper/SourceParserRequestMapperTest.php
+++ tests/CodebaseTest/Infrastructure/CodeBaseReader/Mapper/SourceParserRequestMapperTest.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace CodebaseTest\Infrastructure\CodeBaseReader;
+namespace CodebaseTest\Infrastructure\CodeBaseReader\Mapper;
 
 use Codebase\Application\Dto\CodeBaseRequestDto;
 use Codebase\Application\Dto\ConfigurationResponseDto;
