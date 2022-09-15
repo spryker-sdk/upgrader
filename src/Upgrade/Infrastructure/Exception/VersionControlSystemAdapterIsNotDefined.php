33cafd47dbdd0675a11afadab76b6bce05a3de2f
Dmytro Klyman <dmytro.klyman@spryker.com>
2022-09-15 16:27:40 +0200

    Change the position of strict types declaration in file

--- src/Upgrade/Infrastructure/Exception/VersionControlSystemAdapterIsNotDefined.php
+++ src/Upgrade/Infrastructure/Exception/VersionControlSystemAdapterIsNotDefined.php
@@ -1,11 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
-
-declare(strict_types=1);
 
 namespace Upgrade\Infrastructure\Exception;
 
