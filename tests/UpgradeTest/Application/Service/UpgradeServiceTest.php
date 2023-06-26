7ee56985ab7960c95493e895f525592bdb442b0c
upgrader <sergey.romankov@spryker.com>
2023-06-26 12:43:38 +0300

    SDK-2481 test update

--- tests/UpgradeTest/Application/Service/UpgradeServiceTest.php
+++ tests/UpgradeTest/Application/Service/UpgradeServiceTest.php
@@ -9,11 +9,13 @@ declare(strict_types=1);
 
 namespace UpgradeTest\Application\Service;
 
+use Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Upgrade\Application\Event\UpgraderEventFactory;
 use Upgrade\Application\Service\UpgradeService;
+use Upgrade\Application\Strategy\StrategyInterface;
 use Upgrade\Application\Strategy\StrategyResolver;
 use Upgrade\Infrastructure\Configuration\ConfigurationProvider;
 
@@ -45,5 +47,40 @@ class UpgradeServiceTest extends KernelTestCase
         OUIPUT,
             $res->getOutputMessage(),
         );
+    }
+
+    /**
+     * @return void
+     */
+    public function testUpgradeShouldCollectExceptionWhenItThrows(): void
+    {
+        $this->expectException(Exception::class);
+
+        $configurationProviderMock = $this->createMock(ConfigurationProvider::class);
+        $configurationProviderMock->method('getUpgradeStrategy')->willReturn('composer');
+        $eventDispatcherMock = $this->createMock(EventDispatcherInterface::class);
+        $upgraderEventFactory = $this->createMock(UpgraderEventFactory::class);
+
+        $strategy = $this->createMock(StrategyInterface::class);
+        $strategy->method('upgrade')->willThrowException(new Exception('some_error'));
+
+        $strategyResolver = $this->createMock(StrategyResolver::class);
+        $strategyResolver->method('getStrategy')->willReturn($strategy);
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $service = new UpgradeService($configurationProviderMock, $strategyResolver, $logger, $eventDispatcherMock, $upgraderEventFactory);
+        $service->upgrade();
+    }
+
+    /**
+     * @return \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
+     */
+    public function createEventDispatcherMock(): EventDispatcherInterface
+    {
+        $eventDispatcher = $this->createMock(EventDispatcherInterface::class);
+        $eventDispatcher->expects($this->exactly(2))->method('dispatch');
+
+        return $eventDispatcher;
     }
 }
