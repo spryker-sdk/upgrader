b170a989330c318b73bc60ddd15b4dbc6914bb30
Dmytro Klyman <dmytro.klyman@spryker.com>
2023-08-25 16:05:41 +0200

    Increate test coverage on the project

---
+++ tests/UpgradeTest/Infrastructure/VersionControlSystem/SourceCodeProvider/GitLab/GitLabClientFactoryTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace UpgradeTest\Infrastructure\VersionControlSystem\SourceCodeProvider\GitLab;
+
+use Gitlab\Client as GitLabClient;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Upgrade\Infrastructure\Configuration\ConfigurationProvider;
+use Upgrade\Infrastructure\VersionControlSystem\SourceCodeProvider\GitLab\GitLabClientFactory;
+
+class GitLabClientFactoryTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testGetClient()
+    {
+        $accessToken = 'your_access_token';
+        $sourceCodeProviderUrl = 'https://your.gitlab.url';
+
+        // Create mock for ConfigurationProvider
+        $configurationProviderMock = $this->getMockBuilder(ConfigurationProvider::class)
+            ->getMock();
+        $configurationProviderMock->method('getAccessToken')->willReturn($accessToken);
+        $configurationProviderMock->method('getSourceCodeProviderUrl')->willReturn($sourceCodeProviderUrl);
+
+        // Create mock for ClientInterface
+        $httpClientMock = $this->getMockBuilder(ClientInterface::class)
+            ->getMock();
+
+        $gitLabClientFactory = new GitLabClientFactory($configurationProviderMock, $httpClientMock);
+
+        // Test the first call to getClient() - should create a new instance
+        $client = $gitLabClientFactory->getClient();
+        $this->assertInstanceOf(GitLabClient::class, $client);
+
+        // Test the second call to getClient() - should return the same instance
+        $sameClient = $gitLabClientFactory->getClient();
+        $this->assertSame($client, $sameClient);
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateClient()
+    {
+        $accessToken = 'your_access_token';
+        $sourceCodeProviderUrl = 'https://your.gitlab.url';
+
+        // Create mock for ConfigurationProvider
+        $configurationProviderMock = $this->getMockBuilder(ConfigurationProvider::class)
+            ->getMock();
+        $configurationProviderMock->method('getAccessToken')->willReturn($accessToken);
+        $configurationProviderMock->method('getSourceCodeProviderUrl')->willReturn($sourceCodeProviderUrl);
+
+        // Create mock for ClientInterface
+        $httpClientMock = $this->getMockBuilder(ClientInterface::class)
+            ->getMock();
+
+        $gitLabClientFactory = new GitLabClientFactory($configurationProviderMock, $httpClientMock);
+
+        // Call createClient
+        $client = $gitLabClientFactory->getClient();
+
+        // Assertions
+        $this->assertInstanceOf(GitLabClient::class, $client);
+    }
+}
--- tests/UpgradeTest/Infrastructure/VersionControlSystem/SourceCodeProvider/GitLab/GitLabSourceCodeProviderTest.php
+++ tests/UpgradeTest/Infrastructure/VersionControlSystem/SourceCodeProvider/GitLab/GitLabSourceCodeProviderTest.php
@@ -9,10 +9,12 @@ declare(strict_types=1);
 
 namespace UpgradeTest\Infrastructure\VersionControlSystem\SourceCodeProvider\GitLab;
 
-use Gitlab\Client;
+use Gitlab\Client as GitLabClient;
 use PHPUnit\Framework\TestCase;
 use Upgrade\Application\Dto\StepsResponseDto;
+use Upgrade\Domain\ValueObject\Error;
 use Upgrade\Infrastructure\Configuration\ConfigurationProvider;
+use Upgrade\Infrastructure\VersionControlSystem\Dto\PullRequestDto;
 use Upgrade\Infrastructure\VersionControlSystem\SourceCodeProvider\GitLab\GitLabClientFactory;
 use Upgrade\Infrastructure\VersionControlSystem\SourceCodeProvider\GitLab\GitLabSourceCodeProvider;
 
@@ -75,8 +77,66 @@ class GitLabSourceCodeProviderTest extends TestCase
     protected function createGitLabClientFactoryMock(): GitLabClientFactory
     {
         $gitLabClientFactory = $this->createMock(GitLabClientFactory::class);
-        $gitLabClientFactory->method('getClient')->willReturn($this->createMock(Client::class));
+        $gitLabClientFactory->method('getClient')->willReturn($this->createMock(GitLabClient::class));
 
         return $gitLabClientFactory;
+    }
+
+    /**
+     * @return array<array<mixed>>
+     */
+    public function createPullRequestDataProvider()
+    {
+        return [
+            // Invalid credentials
+            ['', '', '', '', 'Please check defined values of environment variables: ACCESS_TOKEN and (PROJECT_ID or (ORGANIZATION_NAME and REPOSITORY_NAME)).'],
+            ['access_token', '', '', '', 'Please check defined values of environment variables: ACCESS_TOKEN and (PROJECT_ID or (ORGANIZATION_NAME and REPOSITORY_NAME)).'],
+            ['access_token', 'project_id', '', '', ''],
+            ['access_token', '', 'org_name', 'repo_name', ''],
+            // Add more test cases as needed
+        ];
+    }
+
+    /**
+     * @dataProvider createPullRequestDataProvider
+     */
+    public function testCreatePullRequest($accessToken, $projectId, $orgName, $repoName, $expectedError)
+    {
+        // Set up mocks and dependencies
+
+        $configurationProviderMock = $this->createMock(ConfigurationProvider::class);
+        $gitLabClientFactoryMock = $this->createMock(GitLabClientFactory::class);
+
+        $gitLabSourceCodeProvider = new GitLabSourceCodeProvider(
+            $configurationProviderMock,
+            $gitLabClientFactoryMock
+        );
+
+        // Configure the mocks
+        $configurationProviderMock->method('getAccessToken')->willReturn($accessToken);
+        $configurationProviderMock->method('getProjectId')->willReturn($projectId);
+        $configurationProviderMock->method('getOrganizationName')->willReturn($orgName);
+        $configurationProviderMock->method('getRepositoryName')->willReturn($repoName);
+
+        // Create mock DTOs
+        $stepsExecutionDto = new StepsResponseDto();
+        $pullRequestDto = new PullRequestDto('', '', '');
+
+        // Create a mock for the GitLab client and its methods used in the createPullRequest method
+        $gitLabClientMock = $this->getMockBuilder(GitLabClient::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $gitLabClientFactoryMock->method('getClient')->willReturn($gitLabClientMock);
+
+        if ($expectedError) {
+            $stepsExecutionDto = $gitLabSourceCodeProvider->createPullRequest($stepsExecutionDto, $pullRequestDto);
+            $this->assertFalse($stepsExecutionDto->getIsSuccessful());
+            $this->assertInstanceOf(Error::class, $stepsExecutionDto->getError());
+            $this->assertEquals($expectedError, $stepsExecutionDto->getError()->getErrorMessage());
+        } else {
+            $this->assertTrue($stepsExecutionDto->getIsSuccessful());
+            $this->assertNull($stepsExecutionDto->getError());
+        }
     }
 }
