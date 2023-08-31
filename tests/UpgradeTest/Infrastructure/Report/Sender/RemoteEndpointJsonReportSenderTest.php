97b727406ba8e7fc4d42a7713e8bfa11a5e57a9d
Dmytro Klyman <dmytro.klyman@spryker.com>
2023-08-31 13:55:01 +0200

    Add command and sender tests

---
+++ tests/UpgradeTest/Infrastructure/Report/Sender/RemoteEndpointJsonReportSenderTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace UpgradeTest\Infrastructure\Report\Sender;
+
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Upgrade\Application\Provider\ConfigurationProviderInterface;
+use Upgrade\Infrastructure\Report\Dto\ReportDto;
+use Upgrade\Infrastructure\Report\Dto\ReportMetadataDto;
+use Upgrade\Infrastructure\Report\Dto\ReportPayloadDto;
+use Upgrade\Infrastructure\Report\Sender\RemoteEndpointJsonReportSender;
+
+class RemoteEndpointJsonReportSenderTest extends TestCase
+{
+    /**
+     * @return ReportMetadataDto
+     */
+    protected function createReportMetadataDto(): ReportMetadataDto
+    {
+        return new ReportMetadataDto(
+            'organisation_name',
+            'project_name',
+            'project_version',
+            'github',
+            'test',
+            'report_id'
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testSendReport(): void
+    {
+        // Create mock instances for dependencies
+        $httpClient = $this->createMock(ClientInterface::class);
+        $serializer = $this->createMock(SerializerInterface::class);
+        $configurationProvider = $this->createMock(ConfigurationProviderInterface::class);
+
+        // Create an instance of ReportDto (or use a mock)
+        $reportDto = new ReportDto(
+            'your_report_name',
+            1,
+            'scope',
+            new DateTimeImmutable(),
+            new ReportPayloadDto(),
+            $this->createReportMetadataDto()
+        );
+
+        // Configure the serializer mock
+        $serializer->expects($this->once())
+            ->method('serialize')
+            ->with($reportDto, 'json')
+            ->willReturn('{"data": "serialized_report_data"}');
+
+        // Configure the configuration provider mock
+        $configurationProvider->expects($this->once())
+            ->method('getReportSendAuthToken')
+            ->willReturn('your_auth_token');
+
+        // Configure the http client mock
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                'POST',
+                'your_endpoint_url',
+                [
+                    'query' => ['token' => 'your_auth_token'],
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => '{"data": "serialized_report_data"}',
+                    'timeout' => 10, // Example value, adjust as needed
+                    'connect_timeout' => 5, // Example value, adjust as needed
+                ]
+            );
+
+        // Create an instance of the RemoteEndpointJsonReportSender and inject mocks
+        $reportSender = new RemoteEndpointJsonReportSender(
+            $httpClient,
+            $serializer,
+            $configurationProvider,
+            'your_endpoint_url',
+            10, // Example value, adjust as needed
+            5 // Example value, adjust as needed
+        );
+
+        // Call the send method
+        $reportSender->send($reportDto);
+    }
+}
---
+++ tests/Upgrader/Commands/Upgrade/UpgradeCommandTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace UpgraderTest\Commands\Upgrade;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Upgrade\Application\Dto\StepsResponseDto;
+use Upgrade\Application\Service\UpgradeServiceInterface;
+use Upgrader\Console\UpgraderConsole;
+
+class UpgradeCommandTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testExecuteSuccessfulUpgrade(): void
+    {
+        // Create a mock of the UpgradeServiceInterface
+        $upgradeService = $this->createMock(UpgradeServiceInterface::class);
+
+        // Configure the mock to return a successful result
+        $upgradeService->expects($this->once())
+            ->method('upgrade')
+            ->willReturn($this->createSuccessfulExecutionDto());
+
+        // Create a mock for InputInterface and OutputInterface
+        $input = $this->createMock(InputInterface::class);
+        $output = $this->createMock(OutputInterface::class);
+
+        // Create an instance of the UpgraderConsole and inject the mock UpgradeServiceInterface
+        $upgraderConsole = new UpgraderConsole($upgradeService);
+
+        // Call the execute method
+        $result = $upgraderConsole->execute($input, $output);
+
+        // Assert that the result is a success
+        $this->assertSame(UpgraderConsole::SUCCESS, $result);
+    }
+
+    /**
+     * @return void
+     */
+    public function testExecuteFailedUpgrade(): void
+    {
+        // Create a mock of the UpgradeServiceInterface
+        $upgradeService = $this->createMock(UpgradeServiceInterface::class);
+
+        // Configure the mock to return a failed result
+        $upgradeService->expects($this->once())
+            ->method('upgrade')
+            ->willReturn($this->createFailedExecutionDto());
+
+        // Create a mock for InputInterface and OutputInterface
+        $input = $this->createMock(InputInterface::class);
+        $output = $this->createMock(OutputInterface::class);
+
+        // Create an instance of the UpgraderConsole and inject the mock UpgradeServiceInterface
+        $upgraderConsole = new UpgraderConsole($upgradeService);
+
+        // Call the execute method
+        $result = $upgraderConsole->execute($input, $output);
+
+        // Assert that the result is a failure
+        $this->assertSame(UpgraderConsole::FAILURE, $result);
+    }
+
+    /**
+     * @return StepsResponseDto
+     */
+    private function createSuccessfulExecutionDto(): StepsResponseDto
+    {
+        $dto = new StepsResponseDto();
+        $dto->setIsSuccessful(true);
+
+        return $dto;
+    }
+
+    /**
+     * @return StepsResponseDto
+     */
+    private function createFailedExecutionDto(): StepsResponseDto
+    {
+        $dto = new StepsResponseDto();
+        $dto->setIsSuccessful(false);
+
+        return $dto;
+    }
+}
