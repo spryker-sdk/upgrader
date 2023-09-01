e980a871753b197da0aea9a57d99bd8d885cffe2
v.strelchenko <v.strelchenko@gamingtec.com>
2023-09-01 09:57:14 +0300

    SDK-2605: OrmModelsRegenerateStep was added

--- tests/UpgradeTest/Application/Strategy/Common/Step/OrmModelsRegenerateStepTest.php
+++ tests/UpgradeTest/Application/Strategy/Common/Step/OrmModelsRegenerateStepTest.php
@@ -10,8 +10,10 @@ declare(strict_types=1);
 namespace UpgradeTest\Application\Strategy\Common\Step;
 
 use Core\Infrastructure\Service\ProcessRunnerService;
+use Core\Infrastructure\Service\ProcessRunnerServiceInterface;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
 use Upgrade\Application\Dto\ComposerLockDiffDto;
 use Upgrade\Application\Dto\StepsResponseDto;
 use Upgrade\Application\Strategy\Common\Step\OrmModelsRegenerateStep;
@@ -19,10 +21,10 @@ use Upgrade\Domain\Entity\Package;
 
 class OrmModelsRegenerateStepTest extends TestCase
 {
- /**
-  * @return void
-  */
-    public function testRunWhenComposerLockDiffExists(): void
+    /**
+     * @return void
+     */
+    public function testRunSuccess(): void
     {
         // Arrange
         $stepsExecutionDto = new StepsResponseDto(true);
@@ -59,12 +61,47 @@ class OrmModelsRegenerateStepTest extends TestCase
     }
 
     /**
+     * @return void
+     */
+    public function testRunWhenCommandFailed(): void
+    {
+        // Arrange
+        $stepsExecutionDto = new StepsResponseDto(true);
+        $stepsExecutionDto->setComposerLockDiff($this->createComposerLockDiffDto());
+        $processRunnerMock = $this->createProcessRunnerServiceMock();
+        $step = new OrmModelsRegenerateStep($processRunnerMock, new Filesystem());
+
+        // Act
+        $response = $step->run($stepsExecutionDto);
+
+        // Assert
+        $this->assertNotEmpty($response->getBlockers());
+    }
+
+    /**
      * @return \Upgrade\Application\Dto\ComposerLockDiffDto
      */
-    public function createComposerLockDiffDto(): ComposerLockDiffDto
+    protected function createComposerLockDiffDto(): ComposerLockDiffDto
     {
         return new ComposerLockDiffDto([
             new Package('propel/propel', '1.0.1', '1.0.0', 'https://github.com/...'),
         ]);
+    }
+
+    /**
+     * @return \Core\Infrastructure\Service\ProcessRunnerServiceInterface
+     */
+    protected function createProcessRunnerServiceMock(): ProcessRunnerServiceInterface
+    {
+        $process = $this->createMock(Process::class);
+        $process->method('getOutput')->willReturn('Error message');
+        $process->method('getErrorOutput')->willReturn('Error message');
+        $process->method('getExitCode')->willReturn(1);
+
+        $processRunnerService = $this->createMock(ProcessRunnerServiceInterface::class);
+        $processRunnerService->method('mustRun')->willReturn($process);
+        $processRunnerService->method('run')->willReturn($process);
+
+        return $processRunnerService;
     }
 }
