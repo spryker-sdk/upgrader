b923b4bdd41ba361302cd6dec86e6974235eec6b
Sergey Romankov <sergey.romankov@spryker.com>
2023-11-10 12:27:02 +0300

    SDK-5465 rename constant

--- src/ReleaseApp/Infrastructure/Configuration/ConfigurationProvider.php
+++ src/ReleaseApp/Infrastructure/Configuration/ConfigurationProvider.php
@@ -33,7 +33,7 @@ class ConfigurationProvider implements ConfigurationProviderInterface
      */
     public function getReleaseAppUrl(): string
     {
-        return (string)getenv('UPGRADER_RELEASE_APP_URL') ?: static::DEFAULT_RELEASE_APP_URL;
+        return (string)getenv('RELEASE_APP_URL') ?: static::DEFAULT_RELEASE_APP_URL;
     }
 
     /**
