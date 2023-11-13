ef3afa41dcc057afa89aa4ba9fe980abf1784d5b
Vladislav Strelchenko <85889893+VladislavStrelchenko@users.noreply.github.com>
2023-11-13 12:18:57 +0200

    SDK-5464: Add composer command flag (#253)

--- src/Upgrade/Infrastructure/PackageManager/CommandExecutor/ComposerCommandExecutor.php
+++ src/Upgrade/Infrastructure/PackageManager/CommandExecutor/ComposerCommandExecutor.php
@@ -136,9 +136,12 @@ class ComposerCommandExecutor implements ComposerCommandExecutorInterface
     public function updateSubPackage(PackageCollection $packageCollection): PackageManagerResponseDto
     {
         $command = explode(' ', sprintf(
-            '%s%s',
+            '%s%s %s %s %s',
             static::UPDATE_COMMAND_NAME,
             $this->getPackageString($packageCollection),
+            static::NO_SCRIPTS_FLAG,
+            static::NO_PLUGINS_FLAG,
+            static::NO_INTERACTION_FLAG,
         ));
 
         return $this->runWithDependencyFlags($command);
